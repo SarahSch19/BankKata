8e39d9c6e702980f7d6a40f6679219e19ac46c0a
Nigth <omnes.theo@gmail.com>
2021-04-13 21:09:14 +0200

    constructors + getters & setters + tostring

--- bank/Account.java
+++ bank/Account.java
@@ -2,17 +2,58 @@ package bank;
 
 class Account {
 
-    // Attributes
-    // TODO
+    private String name;
+    private int balance;
+    private int threshold;
+    private Boolean blocked;
 
-    // Constructor
-    // TODO
+    public Account(String name, int balance, int threshold) {
+        this.name = name;
+        this.balance = balance;
+        this.threshold = threshold;
+    }
 
     // Methods
     // TODO
 
     public String toString() {
         // TODO
-        return "";
+        return this.getName() + " | " + this.getBalance() + " | " +
+                this.getThreshold() + " | " + this.getBlocked();
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public Boolean getBlocked() {
+        return blocked;
+    }
+
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public void setBlocked(Boolean blocked) {
+        this.blocked = blocked;
     }
 }
